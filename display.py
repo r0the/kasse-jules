c12130d23f7182dfbac5622ead2aea9f942c2e14
Stefan Rothe <stefan@rothe.io>
2021-04-16 15:27:20 +0200

    Added fast refresh for display.

--- display.py
+++ display.py
@@ -3,15 +3,20 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
+DEFAULT_FONT = "/usr/share/fonts/truetype/piboto/Piboto-Regular.ttf"
+
 class Display:
     def __init__(self):
         self.epd = epd4in2.EPD()
         self.image = Image.new('1', (self.epd.width, self.epd.height), 1)
         self.draw = ImageDraw.Draw(self.image)
-        self.font = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf', 36)
+        self.font = ImageFont.truetype(DEFAULT_FONT, 36)
+
+    def clear(self):
+        self.epd.clear()
 
     def text(self, x, y, text):
         self.draw.text((x, y), text, font=self.font, fill=0)
 
-    def show(self):
-        self.epd.show(self.image)
+    def show(self, fast=False):
+        self.epd.show(self.image, fast=fast)
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import display
-
+import time
 
 from PIL import Image
 from PIL import ImageDraw
@@ -8,8 +8,10 @@ from PIL import ImageFont
 
 def main():
     d = display.Display()
+    d.clear()
+    time.sleep(2)
     d.text(10, 10, "Hallo Jules")
-    d.show()
+    d.show(fast=True)
 
 
 if __name__ == '__main__':
