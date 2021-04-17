5b79aeec0c2255284f108d458b2ab288dcb2a18f
Stefan Rothe <stefan@rothe.io>
2021-04-17 14:26:02 +0200

    Work on menu structure

--- camera.py
+++ camera.py
@@ -9,8 +9,17 @@ _camera = picamera.PiCamera();
 _camera.resolution = RESOLUTION
 #_camera.start_preview()
 
+def _image_path(name):
+    return '/home/pi/kasse/images/' + name + '.jpg'
+
 def take_picture():
     stream = io.BytesIO()
     _camera.capture(stream, format='jpeg', resize=RESOLUTION)
     stream.seek(0)
     return PIL.Image.open(stream)
+
+def save_picture(name):
+    _camera.capture(_image_path(name))
+
+def load_picture(name):
+    return PIL.Image.open(_image_path(name))
--- start.py
+++ start.py
@@ -3,43 +3,89 @@ import camera
 import display
 import time
 import keypad
+import subprocess
 
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
-text = ""
+MENU_KEY_X = 10
+MENU_TEXT_X = 50
 
-def take_picture():
-    image = camera.take_picture()
-    display.show_raw(image)
-    time.sleep(10)
+def power_off():
+    subprocess.call("sudo poweroff")
 
-def input_number():
-    global text
-    modified = False
-    for key in keypad.pressed():
-        if key == "*":
-            take_picture()
-        text = text + key
-        modified = True
-    if modified:
-        display.clear()
-        display.text(10, 0, "Kasse")
-        display.text(10, 60, "Version 0.2")
-        display.rectangle(10, 120, 380, 55)
-        display.text(10, 120, text)
-        display.show(fast=True)
-
-def main():
+def register():
     display.clear()
     display.text(10, 0, "Kasse")
-    display.text(10, 60, "Version 0.2")
-    display.show(fast=False)   
+    display.show()
+    global text
     while True:
         keypad.poll()
-        input_number()
-#        time.sleep(0.2)
+        modified = False
+        for key in keypad.pressed():
+            if key == "*":
+                return "main_menu"
+            text = text + key
+            modified = True
+        if modified:
+            display.clear()
+            display.text(MENU_KEY_X, 0, "Kasse")
+            display.rectangle(10, 120, 380, 55)
+            display.text(10, 120, text)
+            display.show(fast=True)
+
+
+def show_menu(menu):
+    display.clear()
+    y = 0
+    for entry in menu:
+        display.text(MENU_KEY_X, y, entry[0])
+        display.text(MENU_TEXT_X, y, entry[1])
+        y = y + 50
+    display.show()
+    while True:
+        keypad.poll()
+        for key in keypad.pressed():
+            for entry in menu:
+                if entry[0] == key:
+                    return entry[2]
+
+text = ""
+
+PICTURE_MENU = [
+    ("1", "Foto aufnehmen", "take"),
+    ("0", "Abbrechen", "abort")
+]
+
+def take_picture():
+    choice = show_menu(PICTURE_MENU)
+    if choice == "take":
+        image = camera.save_picture("splash")
+        display.show_raw(camera.load_picture("splash"))
+        time.sleep(5)
+    return "main_menu"
+
+
+MAIN_MENU = [
+    ("1", "Kasse", "register"),
+    ("2", "Foto", "take_picture"),
+    ("0", "Abschalten", "quit")
+]
+
+def main_menu():
+    return show_menu(MAIN_MENU)
+
+FUNCTIONS = locals()
+
+def main():
+    mode = "main_menu"
+    while mode != "quit":
+        result = FUNCTIONS.get(mode)()
+        if result:
+            mode = result
+    display.show_raw(camera.load_picture("splash"))
+    power_off()
 
 if __name__ == '__main__':
     main()
