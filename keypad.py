3658a1edf8c11906f46091222e8b8061b953d894
Stefan Rothe <stefan@rothe.io>
2021-04-17 11:06:42 +0200

    Keypad improvements.

--- keypad.py
+++ keypad.py
@@ -1,7 +1,7 @@
 import RPi.GPIO as GPIO
 
-ROW_PINS = [12, 13, 14]
-COL_PINS = [15, 16, 17, 18]
+ROW_PINS = [27, 14, 15, 23]
+COL_PINS = [17, 22, 18]
 MATRIX = [
     ["1", "2", "3"],
     ["4", "5", "6"],
@@ -9,13 +9,30 @@ MATRIX = [
     ["*", "0", "#"]
 ]
 
-def _init():
-    for pin in COL_PINS:
-        GPIO.setup(pin, GPIO.OUT)
-    for pin in ROW_PINS:
-        GPIO.setup(pin, GPIO.IN)
+_pressed = []
+_previous = []
 
+for col_pin in COL_PINS:
+    GPIO.setup(col_pin, GPIO.OUT, initial=GPIO.LOW)
+for row_pin in ROW_PINS:
+    GPIO.setup(row_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-def
+def poll():
+    global _pressed
+    global _previous
+    _previous = _pressed
+    _pressed = []
+    for col in range(len(COL_PINS)):
+        GPIO.output(COL_PINS[col], GPIO.HIGH)
+        for row in range(len(ROW_PINS)):
+            if GPIO.input(ROW_PINS[row]):
+                key = MATRIX[row][col]
+                _pressed.append(key)
+        GPIO.output(COL_PINS[col], GPIO.LOW)
 
-_init()
+def pressed():
+    result = []
+    for key in _pressed:
+        if not key in _previous:
+            result.append(key)
+    return result
--- main.py
+++ main.py
@@ -1,17 +1,35 @@
 import display
 import time
+import keypad
 
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
+text = ""
+
+def input_number():
+    global text
+    modified = False
+    for key in keypad.pressed():
+        text = text + key
+        modified = True
+    if modified:
+        display.clear()
+        display.text(10, 0, "Kasse")
+        display.text(10, 60, "Version 0.2")
+        display.text(10, 120, text)
+        display.show(fast=True)
 
 def main():
     display.clear()
-    display.text(10, 10, "Kasse von Jules")
-    display.text(10, 60, "Version 0.1")
-    display.show(fast=True)
-
+    display.text(10, 0, "Kasse")
+    display.text(10, 60, "Version 0.2")
+    display.show(fast=False)   
+    while True:
+        keypad.poll()
+        input_number()
+#        time.sleep(0.2)
 
 if __name__ == '__main__':
     main()
